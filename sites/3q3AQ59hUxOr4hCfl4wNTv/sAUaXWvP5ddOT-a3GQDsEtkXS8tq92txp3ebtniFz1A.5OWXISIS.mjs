e08bed5e26c43ebf37df2c2bcb87a0a8b802df93
zkCryptoBiz <158302791+zkCryptoBiz@users.noreply.github.com>
2024-12-11 10:01:10 +0200

    Update sAUaXWvP5ddOT-a3GQDsEtkXS8tq92txp3ebtniFz1A.5OWXISIS.mjs

--- sites/3q3AQ59hUxOr4hCfl4wNTv/sAUaXWvP5ddOT-a3GQDsEtkXS8tq92txp3ebtniFz1A.5OWXISIS.mjs
+++ sites/3q3AQ59hUxOr4hCfl4wNTv/sAUaXWvP5ddOT-a3GQDsEtkXS8tq92txp3ebtniFz1A.5OWXISIS.mjs
@@ -3365,7 +3365,7 @@ var yt = O(ae),
                               }),
                             }),
                             e(Z, {
-                              href: "https://pump.fun/xxxxxxxxx",
+                              href: "https://pump.fun/3sCz52wn63jrnRwiqTnATbbjPSvyVcZDK4b6coL9pump",
                               openInNewTab: !0,
                               children: e(R, {
                                 breakpoint: p,
@@ -3430,12 +3430,12 @@ var yt = O(ae),
                                     children: e(Be, {
                                       cEGfNMrg4: 15,
                                       GSol4UKtR:
-                                        "xxxxxxxxx",
+                                        "3sCz52wn63jrnRwiqTnATbbjPSvyVcZDK4b6coL9pump",
                                       height: "100%",
                                       id: "cLqKpWUXG",
                                       layoutId: "cLqKpWUXG",
                                       pFBpLpb7W:
-                                        "xxxxxxxxx",
+                                        "3sCz52wn63jrnRwiqTnATbbjPSvyVcZDK4b6coL9pump",
                                       style: { height: "100%", width: "100%" },
                                       variant: "zyJ1yXUEk",
                                       width: "100%",
@@ -3449,7 +3449,7 @@ var yt = O(ae),
                       ],
                     }),
                     e(Z, {
-                      href: "https://pump.fun/xxxxxxxxx",
+                      href: "https://pump.fun/3sCz52wn63jrnRwiqTnATbbjPSvyVcZDK4b6coL9pump",
                       openInNewTab: !0,
                       children: e(c.a, {
                         className: "framer-1au8r0f framer-lux5qc",
