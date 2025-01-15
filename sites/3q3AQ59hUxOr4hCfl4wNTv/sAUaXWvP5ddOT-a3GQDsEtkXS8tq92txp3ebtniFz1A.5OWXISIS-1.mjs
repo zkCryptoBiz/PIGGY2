f65bef9744840c5b96bfa86cabc2aaa68145cbd9
zkCryptoBiz <158302791+zkCryptoBiz@users.noreply.github.com>
2025-01-15 12:19:49 +0200

    Update sAUaXWvP5ddOT-a3GQDsEtkXS8tq92txp3ebtniFz1A.5OWXISIS-1.mjs

--- sites/3q3AQ59hUxOr4hCfl4wNTv/sAUaXWvP5ddOT-a3GQDsEtkXS8tq92txp3ebtniFz1A.5OWXISIS-1.mjs
+++ sites/3q3AQ59hUxOr4hCfl4wNTv/sAUaXWvP5ddOT-a3GQDsEtkXS8tq92txp3ebtniFz1A.5OWXISIS-1.mjs
@@ -3366,7 +3366,7 @@ var yt = O(ae),
                             }),
                             e(Z, 
                                 {
-                              href: "https://pump.fun/contractcontract",
+                              href: "https://pump.fun/6dDgXek1FPqRJLs5GG1A4AL4LMmsygCeDjWv67rppump",
                               openInNewTab: !0,
                               children: e(R, {
                                 breakpoint: p,
@@ -3468,12 +3468,12 @@ var yt = O(ae),
                                     children: e(Be, {
                                       cEGfNMrg4: 15,
                                       GSol4UKtR:
-                                        "contractcontract",
+                                        "6dDgXek1FPqRJLs5GG1A4AL4LMmsygCeDjWv67rppump",
                                       height: "100%",
                                       id: "cLqKpWUXG",
                                       layoutId: "cLqKpWUXG",
                                       pFBpLpb7W:
-                                        "contractcontract",
+                                        "6dDgXek1FPqRJLs5GG1A4AL4LMmsygCeDjWv67rppump",
                                       style: { height: "100%", width: "100%" },
                                       variant: "zyJ1yXUEk",
                                       width: "100%",
@@ -3487,7 +3487,7 @@ var yt = O(ae),
                       ],
                     }),
                     e(Z, {
-                      href: "https://jup.ag/swap/contractcontract-SOL",
+                      href: "https://jup.ag/swap/6dDgXek1FPqRJLs5GG1A4AL4LMmsygCeDjWv67rppump-SOL",
                       openInNewTab: !0,
                       children: e(c.a, {
                         className: "framer-1au8r0f framer-lux5qc",
